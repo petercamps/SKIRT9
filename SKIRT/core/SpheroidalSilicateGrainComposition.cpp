9c2d26e74d96957833bca654a802b4529a43427d
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2020-10-23 15:15:43 +0200

    Added additional configuration attributes for SpheroidalSilicateGrainComposition.

--- SKIRT/core/SpheroidalSilicateGrainComposition.cpp
+++ SKIRT/core/SpheroidalSilicateGrainComposition.cpp
@@ -4,6 +4,21 @@
 ///////////////////////////////////////////////////////////////// */
 
 #include "SpheroidalSilicateGrainComposition.hpp"
+#include "FatalError.hpp"
+
+////////////////////////////////////////////////////////////////////
+
+void SpheroidalSilicateGrainComposition::setupSelfBefore()
+{
+
+    PolarizedSilicateGrainComposition::setupSelfBefore();
+
+    if (!_alignedEmissionTable.empty() && _alignmentFraction != 1. && _nonAlignedEmissionTable.empty())
+    {
+        throw FATALERROR(
+            "No custom emission table provided for non-aligned grains, yet the alignment fraction is not 1!");
+    }
+}
 
 ////////////////////////////////////////////////////////////////////
 
@@ -18,10 +33,32 @@ bool SpheroidalSilicateGrainComposition::resourcesForSpheroidalEmission(bool& re
                                                                         std::string& tableName1,
                                                                         std::string& tableName2) const
 {
-    resource = false;
-    interpol = 0.;
-    tableName1 = _spheroidalEmissionTable;
-    tableName2 = string();
+
+    if (!_alignedEmissionTable.empty())
+    {
+        resource = false;
+        if (!_nonAlignedEmissionTable.empty())
+        {
+            interpol = _alignmentFraction;
+            tableName1 = _nonAlignedEmissionTable;
+            tableName2 = _alignedEmissionTable;
+        }
+        else
+        {
+            interpol = 0;
+            tableName1 = _alignedEmissionTable;
+            tableName2 = string();
+        }
+    }
+    else
+    {
+        throw FATALERROR("Spheroidal tables are not part of the SKIRT resources yet!");
+        resource = true;
+        interpol = _alignmentFraction;
+        tableName1 = "NAME OF PRECOMPUTED NON-ALIGNED TABLE";
+        tableName2 = "NAME OF PRECOMPUTED ALIGNED TABLE";
+    }
+
     return true;
 }
 
--- SKIRT/core/SpheroidalSilicateGrainComposition.hpp
+++ SKIRT/core/SpheroidalSilicateGrainComposition.hpp
@@ -27,12 +27,30 @@ class SpheroidalSilicateGrainComposition : public PolarizedSilicateGrainComposit
                   "a spheroidal silicate dust grain composition with support for polarization")
         ATTRIBUTE_TYPE_DISPLAYED_IF(SpheroidalSilicateGrainComposition, "Spheroidal")
 
-        PROPERTY_STRING(spheroidalEmissionTable,
-                        "the name of the file tabulating properties for polarized emission by spheroidal grains")
+        PROPERTY_STRING(
+            alignedEmissionTable,
+            "the name of the file tabulating properties for polarized emission by perfectly aligned spheroidal grains")
+        ATTRIBUTE_DEFAULT_VALUE(alignedEmissionTable, "")
+        ATTRIBUTE_REQUIRED_IF(alignedEmissionTable, "false")
+
+        PROPERTY_STRING(
+            nonAlignedEmissionTable,
+            "the name of the file tabulating properties for polarized emission by non-aligned spheroidal grains")
+        ATTRIBUTE_DEFAULT_VALUE(nonAlignedEmissionTable, "")
+        ATTRIBUTE_REQUIRED_IF(nonAlignedEmissionTable, "false")
+
+        PROPERTY_DOUBLE(alignmentFraction,
+                        "the alignment fraction of the spheroidal grains with the local magnetic field")
+        ATTRIBUTE_DEFAULT_VALUE(alignmentFraction, "1.")
+        ATTRIBUTE_MIN_VALUE(alignmentFraction, "0.")
+        ATTRIBUTE_MAX_VALUE(alignmentFraction, "1.")
 
     ITEM_END()
 
 public:
+    /** This function verifies that all attribute values have been appropriately set. */
+    void setupSelfBefore() override;
+
     /** This function returns a brief human-readable identifier for this grain composition. */
     string name() const override;
 
